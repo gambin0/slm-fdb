dd0841cda0c72ceabee3fc9706c4d56e80f392a2
gambin0 <charlesung@gmail.com>
2018-03-20 08:04:08 -0500

    update

---
+++ src/app/measurement-editor/measurement-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-editor',
+  templateUrl: './measurement-editor.component.html',
+  styleUrls: ['./measurement-editor.component.less']
+})
+export class MeasurementEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -3,4 +3,6 @@ import { Item } from './item.model';
 export class Recipe {
     name: string;
     items: Item[];
+    timestamp: Date;
+    link: string;
 }
